fd50c0085966deb06199974a3355c60d82cb0ba2
Simge Nur Cankaya <simgenurcankaya@gmail.com>
2020-03-12 21:55:32 +0300

    v1.1

--- hw1.c
+++ hw1.c
@@ -5,6 +5,7 @@
 #include<string.h> 
 #include<sys/wait.h> 
 #include<sys/socket.h>
+#include<poll.h>
 #include "logging.h"
 #include "message.h"
 #define PIPE(fd) socketpair(AF_UNIX, SOCK_STREAM, PF_UNIX, fd)
@@ -13,12 +14,15 @@
 typedef struct input
 {
     char executable[20];
+    int numberOfArgs;
     char args[20];
     /* data */
 }input;
 
 int execute(input);
 int child(input x, int pipe[]);
+int parent(int p1[],int p2[]);
+
 
 int main(){
     int starting_bid, minimum_increment,number_of_bidders;
@@ -30,12 +34,21 @@ int main(){
     for(int i = 0 ; i<number_of_bidders; i++){
         printf("Scanning...\n");
         scanf(" %s", bidders[i].executable);
+        scanf(" %d", &bidders[i].numberOfArgs);
         scanf(" %[^\n]s",bidders[i].args);
 
     }
 
     int pipe1[2];
     int pipe2[2];
+
+    PIPE(pipe1);
+    PIPE(pipe2);
+    dup2(pipe1[0],0);
+    dup2(pipe1[1],1);
+    dup2(pipe2[0],0);
+    dup2(pipe2[1],1);
+    
     int w;
 
     for( int i = 0 ; i< number_of_bidders; i++){
@@ -45,10 +58,12 @@ int main(){
     if (fork()) {
 		if (fork()) {
             printf("Parent waiting... \n");
+
+            parent(pipe1,pipe2);
 			wait(&w);
 			wait(&w);
 
-            printf("Parent waiting ended...\n Exterminating.");
+            printf("Parent waiting ended...\nExterminating.");
 		} else {
 			return child(bidders[0],pipe1);
 		}
@@ -75,5 +90,23 @@ int execute(input x){
 int child(input x, int pipe[] ){
     printf("Inside child \n" );
     execute(x);
+}
 
+int parent(int p1[],int p2[]){
+	struct pollfd pfd[2] = {{ p1[0], POLLIN, 0}, { p2[0], POLLIN, 0}} ;
+    char mess[40];
+    int n = 2,r;
+
+    while (pfd[0].fd >= 0 || pfd[1].fd >= 0) { /* one still open */
+
+		poll(pfd, n, 0);  /* no timeout*/
+		for (int i = 0; i < n; i++) 
+			if (pfd[i].revents && POLLIN) {
+				r = read(pfd[i].fd, mess, 40);
+				if (r == 0) 			/* EOF */
+					pfd[i].fd = -1;   /* poll() ignores pollfd item if fd is negative */
+				else
+					printf("%d: %s\n", i, mess);
+			}
+	}
 }
